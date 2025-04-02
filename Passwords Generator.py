ad28b53a6f88529a069a94451aa811751f13b98f
DORORO <rabie.malouli.computer@gmail.com>
2025-04-02 21:01:03 +0000

    change variable name and ask user

--- Passwords Generator.py
+++ Passwords Generator.py
@@ -1,59 +1,61 @@
 # import modules
 import string
 import random
+
 print("----------Welcome To Password Generator----------")
+
 while True:
-# store all characters in list
-    string_1 = list(string.ascii_lowercase)
-    string_2 = list(string.ascii_uppercase)
-    string_3 = list(string.digits)
-    string_4 = list(string.punctuation)
+    # Reset password list for each generation
+    password = []
+
+    # store all characters in list
+    lower_chars = list(string.ascii_lowercase)
+    upper_chars = list(string.ascii_uppercase)
+    digits = list(string.digits)
+    punctuation = list(string.punctuation)
 
     # shuffle all lists
-    random.shuffle(string_1)
-    random.shuffle(string_2)
-    random.shuffle(string_3)
-    random.shuffle(string_4)
+    random.shuffle(lower_chars)
+    random.shuffle(upper_chars)
+    random.shuffle(digits)
+    random.shuffle(punctuation)
 
-# make sure the number of characters is 6 or more
+    # make sure the number of characters is 6 or more
     while True:
         try:
-            characters_number = int(input("how many characters for the password?: "))
-            if characters_number <8 :
-                print("you need at least 8 characters")
+            length = int(input("How many characters for the password? (minimum 8): "))
+            if length <8 :
+                print("Password must be at least 8 characters long.")
             else:
                 break
         except:
-            print("please entre number only")
+            print("Please enter a valid number.")
 
-# calculate 40% and 10% of number of characters
-    part_1 = round(characters_number * (40/100))
-    part_2 = round(characters_number * (10/100))
+    # calculate 30% and 20% of number of characters
+    part_1 = round(length * (30/100)) 
+    part_2 = round(length * (20/100))
 
-# generate password
-    password = []
-
+    # generate password
     for row in range(part_1):
-        password.append(string_1[row])
-        password.append(string_2[row])
+        password.append(lower_chars[row])
+        password.append(upper_chars[row])
 
     for row in range(part_2):
-        password.append(string_3[row])
-        password.append(string_4[row])
+        password.append(digits[row])
+        password.append(punctuation[row])
 
-# finally print password
+    # finally print password
     random.shuffle(password)
     password = "".join((password[0:]))
     print(password)
 
-# Ask the user if they want to perform another operation
+    # Ask the user if they want to perform another operation
     while True:
-        ask_user = input("Do you want to perform another operation? [Y/n]: ").strip().lower()
-        if ask_user == "y":
+        repeat = input("Generate another password? [Y/n]: ").strip().lower()
+        if repeat == "y":
             break
-        elif ask_user == "n":
-            print("Program exited.")
+        elif repeat == "n":
+            print("Thank you for using the Password Generator. Goodbye!")
             exit()
         else:
-            print("Invalid input. Please enter 'y' or 'n'.")
-            
+            print("Invalid input. Please enter 'Y' or 'n'.")
