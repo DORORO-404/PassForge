33ed807bdee882dcf50f3ba5139364fabf8ed5ac
DORORO <rabie.malouli.computer@gmail.com>
2025-04-03 22:15:42 +0000

    I organized the code and fixed the errors.

--- Password Generator.py
+++ Password Generator.py
@@ -2,54 +2,56 @@
 import string
 import random
 
-print("----------Welcome To Password Generator----------")
+print("\033[1;31m[+] ====== Welcome To Advanced Password Generator ====== [+]\033[0m")
 
 while True:
     # Reset password list for each generation
     password = []
 
-    # store all characters in list
+    # Character sets
     lower_chars = list(string.ascii_lowercase)
     upper_chars = list(string.ascii_uppercase)
     digits = list(string.digits)
     punctuation = list(string.punctuation)
 
-    # shuffle all lists
+    # Shuffle all sets
     random.shuffle(lower_chars)
     random.shuffle(upper_chars)
     random.shuffle(digits)
     random.shuffle(punctuation)
 
-    # make sure the number of characters is 6 or more
+    # Get password length with validation
     while True:
         try:
-            length = int(input("How many characters for the password? (minimum 8): "))
+            length = int(input("How many characters for the password?: "))
             if length <8 :
                 print("Password must be at least 8 characters long.")
+            elif length > 65:
+                print("Password must be less than 65 characters long.")
             else:
                 break
-        except:
+        except ValueError:
             print("Please enter a valid number.")
 
-    # calculate 30% and 20% of number of characters
-    part_1 = round(length * (30/100)) 
-    part_2 = round(length * (20/100))
+    # Calculate character distribution
+    partie_1 = round(length * (40/100)) 
+    partie_2 = round(length * (10/100))
 
     # generate password
-    for row in range(part_1):
+    for row in range(partie_1):
         password.append(lower_chars[row])
         password.append(upper_chars[row])
 
-    for row in range(part_2):
+    for row in range(partie_2):
         password.append(digits[row])
         password.append(punctuation[row])
 
-    # finally print password
+    # Shuffle and display password
     random.shuffle(password)
-    password = "".join((password[0:]))
-    print(password)
+    final_password = "".join(password)
+    print(f"Generated Password: \"{final_password}\"")
 
-    # Ask the user if they want to perform another operation
+    # Ask to continue
     while True:
         repeat = input("Generate another password? [Y/n]: ").strip().lower()
         if repeat == "y":
@@ -58,4 +60,4 @@ while True:
             print("Thank you for using the Password Generator. Goodbye!")
             exit()
         else:
-            print("Invalid input. Please enter 'Y' or 'n'.")
+            print("Invalid input. Please enter 'y' or 'n'.")
